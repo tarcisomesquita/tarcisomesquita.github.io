2ddb8837feb6746a000cf8e2847ebf06b88172ca
tarcisomesquita <tarcisomesquita@gmail.com>
2024-10-05 22:10:36 -0300

    05-10-2024_22:10:36

---
+++ arquivos/filetypes/webp/WebP Container Specification _ Google Developers_files/devsite_devsite_user_module.js
@@ -0,0 +1,1 @@
+(function(_ds){var window=this;'use strict';try{window.customElements.define("devsite-user",_ds.fI)}catch(a){console.warn("Unrecognized DevSite custom element - DevsiteUser",a)};})(_ds_www);
